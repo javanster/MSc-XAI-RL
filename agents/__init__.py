7b062001c13b36bdff1907517cec627794b4bd07
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-01-13 14:03:12 +0100

    Add export for ObservationNormalizationCallbacks

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,4 @@
 from .custom_types.model_training_config import ModelTrainingConfig
 from .ddqn_agent import DDQNAgent
 from .human_player import HumanPlayer
+from .util_classes.observation_normalization_callbacks import ObservationNormalizationCallbacks
