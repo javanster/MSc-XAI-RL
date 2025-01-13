9dbd6eb292e6981e5c0a0a55f751338c35f30165
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-01-13 10:11:12 +0100

    Add package exports

---
+++ rl_tcav/__init__.py
@@ -0,0 +1,6 @@
+from .concept_classes.binary_concept import BinaryConcept
+from .concept_example_collector_classes.binary_concept_example_collector import (
+    BinaryConceptExampleCollector,
+)
+from .concept_probes.binary_concept_probe import BinaryConceptProbe
+from .model_activation_obtainer import ModelActivationObtainer
