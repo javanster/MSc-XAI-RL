2047e37f87279eb7242a2b4a27e5b84d038a52f4
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-01-28 09:11:22 +0100

    Add new script for car racing

--- scripts/ccr/ccr_play.py
+++ scripts/ccr/ccr_play.py
@@ -1,10 +1,11 @@
+import ccr
 import gymnasium as gym
 import numpy as np
 from gymnasium.utils.play import play
 
 if __name__ == "__main__":
     play(
-        gym.make("CarRacing-v3", render_mode="rgb_array"),
+        gym.make("CCR-v5", render_mode="rgb_array"),
         keys_to_action={
             "w": np.array([0, 0.7, 0], dtype=np.float32),
             "a": np.array([-1, 0, 0], dtype=np.float32),
