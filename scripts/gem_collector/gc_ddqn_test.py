af247847d28303839d2d6ad442bc9643f3ca1562
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-06 00:02:02 +0100

    Model file path variable in test script

--- scripts/gem_collector/gc_ddqn_test.py
+++ scripts/gem_collector/gc_ddqn_test.py
@@ -7,11 +7,11 @@ from keras.api.saving import load_model
 
 from agents import DDQNAgent
 
+MODEL_FILE_PATH = "models/GemCollector/northern-sweep-19/1738791493_model____0.3843avg____0.5330max____0.2126min.keras"
+
 if __name__ == "__main__":
     env = gym.make(id="GemCollector-v3", render_mode="human", show_raw_pixels=False)
     agent = DDQNAgent(env=env, obervation_normalization_type="image")
-    model = load_model(
-        filepath="models/GemCollector/northern-sweep-19/1738791493_model____0.3843avg____0.5330max____0.2126min.keras"
-    )
+    model = load_model(filepath=MODEL_FILE_PATH)
     model = cast(Sequential, model)
     agent.test(model=model, episodes=1)
