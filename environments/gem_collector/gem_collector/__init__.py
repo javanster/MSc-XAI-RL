d42c2d7dc674ece5fd54645bf5f6828215b134d9
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-05 20:45:09 +0100

    Update env to v3

--- environments/gem_collector/gem_collector/__init__.py
+++ environments/gem_collector/gem_collector/__init__.py
@@ -1,7 +1,7 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="GemCollector-v2",
+    id="GemCollector-v3",
     entry_point="gem_collector.envs:GemCollector",
     max_episode_steps=190,
 )
--- environments/gem_collector/setup.py
+++ environments/gem_collector/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gem_collector",
-    version="2.0.9",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[
         "gymnasium",
