88f4cc7c34511ddf8ff988b5b75f3847bfeaa5e0
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-11 16:03:42 +0100

    Move manual control agent to agents

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,4 @@
+from .box_escape_human_player import BoxEscapeHumanPlayer
 from .custom_types.model_training_config import ModelTrainingConfig
 from .ddqn_agent import DDQNAgent
 from .human_player import HumanPlayer
---
+++ agents/box_escape_human_player.py
@@ -0,0 +1,72 @@
+from enum import IntEnum
+
+from minigrid.manual_control import ManualControl
+
+
+class Actions(IntEnum):
+    """
+    The possible actions in the BoxEscape environment.
+
+    These actions define the discrete movement and interaction capabilities of
+    the agent as it navigates the environment.
+
+    Attributes
+    ----------
+    left : int
+        Rotate the agent 90 degrees to the left.
+    right : int
+        Rotate the agent 90 degrees to the right.
+    forward : int
+        Move the agent one step forward in the direction it is facing.
+    pickup : int
+        Pick up an object (such as a key or a box) if the agent is facing it.
+    toggle : int
+        Toggle or interact with an object, such as opening a door or activating a switch.
+    """
+
+    left = 0
+    right = 1
+    forward = 2
+    pickup = 3
+    toggle = 4
+
+
+class BoxEscapeHumanPlayer(ManualControl):
+    """
+    A custom manual control interface for interacting with the BoxEscape environment.
+
+    This class extends the standard ManualControl class and maps keyboard inputs
+    to predefined actions in the environment.
+    """
+
+    def key_handler(self, event):
+        """
+        Handles keyboard inputs and translates them into environment actions.
+
+        Parameters
+        ----------
+        event : pygame.event.Event
+            The keyboard event triggered by the user.
+        """
+        key: str = event.key
+        print("pressed", key)
+
+        if key == "escape":
+            self.env.close()
+            return
+        if key == "backspace":
+            self.reset()
+            return
+
+        key_to_action = {
+            "left": Actions.left,
+            "right": Actions.right,
+            "up": Actions.forward,
+            "tab": Actions.pickup,
+            "space": Actions.toggle,
+        }
+        if key in key_to_action.keys():
+            action = key_to_action[key]
+            self.step(action)
+        else:
+            print(key)
