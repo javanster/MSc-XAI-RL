b93c4537f85f6cf3844ebc546236b3d38bf0f3ad
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-21 10:40:06 +0100

    ccr update

--- environments/ccr/setup.py
+++ environments/ccr/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ccr",
-    version="5.0.1",
+    version="5.0.2",
     packages=find_packages(),
     install_requires=[
         "gymnasium",
