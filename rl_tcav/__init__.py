b01abd444a2e04a1d38c36e5569edd6a098e41aa
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-23 00:49:37 +0100

    Add rl tcav exports

--- rl_tcav/__init__.py
+++ rl_tcav/__init__.py
@@ -1,7 +1,15 @@
 from .cav import Cav
 from .concept_classes.binary_concept import BinaryConcept
+from .concept_classes.continuous_concept import ContinuousConcept
 from .concept_example_collector_classes.binary_concept_example_collector import (
     BinaryConceptExampleCollector,
 )
+from .concept_example_collector_classes.continuous_concept_example_collector import (
+    ContinuousConceptExampleCollector,
+)
 from .concept_probes.binary_concept_probe import BinaryConceptProbe
 from .model_activation_obtainer import ModelActivationObtainer
+from .tcav_example_collector_classes.tcav_model_policy_example_collector import (
+    TcavModelPolicyExampleCollector,
+)
+from .tcav_score_calculator import TcavScoreCalculator
