cc682376ad46e9bb3fe40fdbcdf1ab0d6e35302b
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-02-24 12:57:07 +0100

    Delete unused scripts

--- scripts/fr2d/fr2d_play.py
+++
@@ -1,8 +0,0 @@
-import gymnasium
-from minigrid.manual_control import ManualControl
-
-if __name__ == "__main__":
-    env = gymnasium.make("MiniGrid-FourRooms-v0", render_mode="human")
-
-    manual_control = ManualControl(env)
-    manual_control.start()
