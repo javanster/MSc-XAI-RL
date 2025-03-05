5a5f31f388cdb79accb3091449a3895679e08450
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-03-05 12:03:59 +0100

    Constants, TCAV GemCollector

--- scripts/gem_collector/tcav/concept_example_collection/constants.py
+++ scripts/gem_collector/tcav/concept_example_collection/constants.py
@@ -1,5 +1,3 @@
-MODEL_OF_INTEREST_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
-MORE_CAPABLE_MODEL_PATH = "models/GemCollector/iconic-sweep-77/1738860819_model____0.6935avg____0.7910max____0.4690min.keras"
 EXAMPLE_DATA_DIRECTORY_PATH = "rl_tcav_data/concept_examples/gem_collector"
 ENV_NAME = "gem_collector"
 EXAMPLE_N = 100_000
---
+++ scripts/gem_collector/tcav/constants.py
@@ -0,0 +1,3 @@
+MODEL_OF_INTEREST_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
+MORE_CAPABLE_MODEL_PATH = "models/GemCollector/iconic-sweep-77/1738860819_model____0.6935avg____0.7910max____0.4690min.keras"
+MODEL_OF_INTEREST_NAME = "denim-sweep-56"
