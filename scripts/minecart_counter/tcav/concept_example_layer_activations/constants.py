656cb173a7e9051a3e310ebffcd2e34858c04fc5
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-03-05 11:48:33 +0100

    TCAV minecart counter constants

---
+++ scripts/minecart_counter/tcav/concept_example_layer_activations/constants.py
@@ -0,0 +1,7 @@
+CONCEPT_EXAMPLE_PATHS = {
+    "random_policy": "rl_tcav_data/concept_examples/minecart_counter/random_policy_play",
+    "model_of_interest_greedy": "rl_tcav_data/concept_examples/minecart_counter/model_of_interest_greedy_play",
+    "model_of_interest_greedy_epsilon0.05": "rl_tcav_data/concept_examples/minecart_counter/model_of_interest_epsilon0_005_play",
+    "more_capable_model_greedy": "rl_tcav_data/concept_examples/minecart_counter/more_capable_model_greedy_play",
+    "more_capable_model_greedy_epsilon0.05": "rl_tcav_data/concept_examples/minecart_counter/more_capable_model_epsilon0_005_play",
+}
---
+++ scripts/minecart_counter/tcav/constants.py
@@ -0,0 +1,3 @@
+MODEL_OF_INTEREST_PATH = "models/MinecartCounter/scattered/kind-cosmos-35/1740021317_model____0.5122avg____0.9750max___-0.0900min.keras"
+MORE_CAPABLE_MODEL_PATH = "models/MinecartCounter/scattered/genial-brook-15/model_time_step_420321_episode_43750____0.9757avg____0.9800max____0.9650min.keras"
+MODEL_OF_INTEREST_NAME = "kind-cosmos-35"
