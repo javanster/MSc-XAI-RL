b4c67d73bf05c3385688a40dcf66bc1c0815894d
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-03-05 11:59:41 +0100

    Constants, TCAV GoldRun

---
+++ scripts/gold_run_mini/tcav/concept_example_layer_activations/constants.py
@@ -0,0 +1,8 @@
+CONCEPT_EXAMPLE_PATHS = {
+    "random_policy": "rl_tcav_data/concept_examples/gold_run_mini/random_policy_play",
+    "model_of_interest_greedy": "rl_tcav_data/concept_examples/gold_run_mini/model_of_interest_greedy_play",
+    "model_of_interest_greedy_epsilon0.05": "rl_tcav_data/concept_examples/gold_run_mini/model_of_interest_epsilon0_005_play",
+    "more_capable_model_greedy": "rl_tcav_data/concept_examples/gold_run_mini/more_capable_model_greedy_play",
+    "more_capable_model_greedy_epsilon0.05": "rl_tcav_data/concept_examples/gold_run_mini/more_capable_model_epsilon0_005_play",
+}
+ACTIVATIONS_N = 1_000
---
+++ scripts/gold_run_mini/tcav/constants.py
@@ -0,0 +1,3 @@
+MODEL_OF_INTEREST_PATH = "models/GoldRunMini/sub-competent/firm-mountain-13/model_time_step_457199_episode_15800____0.4878avg____0.5000max____0.4532min.keras"
+MORE_CAPABLE_MODEL_PATH = "models/GoldRunMini/competent/firm-salad-11/model_time_step_390195_episode_3100____0.8544avg____0.9910max___-0.2000min.keras"
+MODEL_OF_INTEREST_NAME = "firm-mountain-13"
