26506c1ae4c11d16f918c6afb61cee5c1aab1a0d
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-03-08 12:30:44 +0100

    Scripts for training and testing baseline concept probes based on different data collection approaches, GoldRunMini

--- scripts/gold_run_mini/tcav/concept_probe_training/baseline_probe_testing/constants.py
+++ scripts/gold_run_mini/tcav/concept_probe_training/baseline_probe_testing/constants.py
@@ -1,3 +1,11 @@
 CONCEPT_PREFIXES = ["binary_concept_gold_above", "binary_concept_lava_1_above"]
 CONCEPT_NAMES = ["gold_above", "lava_1_above"]
 CONCEPT_DATA_APPROACH_PATH_BASE = "rl_tcav_data/concept_examples/gold_run_mini/"
+VALIDATION_DATASET_PATHS = [
+    "rl_tcav_data/cavs/baseline_concept_probes_experiment/gold_run_mini/validation_sets/gold_above_probe_validation_dataset_method1.npy",
+    "rl_tcav_data/cavs/baseline_concept_probes_experiment/gold_run_mini/validation_sets/lava_1_above_probe_validation_dataset_method3.npy",
+]
+VALIDATION_LABEL_SET_PATHS = [
+    "rl_tcav_data/cavs/baseline_concept_probes_experiment/gold_run_mini/validation_sets/gold_above_probe_validation_label_set_method1.npy",
+    "rl_tcav_data/cavs/baseline_concept_probes_experiment/gold_run_mini/validation_sets/lava_1_above_probe_validation_label_set_method3.npy",
+]
---
+++ scripts/gold_run_mini/tcav/concept_probe_training/baseline_probe_testing/grm_train_baseline_concept_probes_approach_2.py
@@ -0,0 +1,4 @@
+from .grm_train_baseline_concept_probes import obtain_cavs_by_approach
+
+if __name__ == "__main__":
+    obtain_cavs_by_approach("model_of_interest_greedy_play")
