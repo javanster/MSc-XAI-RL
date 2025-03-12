648435be345dfccb88a0789a6a2057f6823c80c8
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-03-12 21:54:52 +0100

    Update validation set sizes

--- scripts/gem_collector/tcav/concept_probe_training/baseline_probe_testing/constants.py
+++ scripts/gem_collector/tcav/concept_probe_training/baseline_probe_testing/constants.py
@@ -1,6 +1,6 @@
 CONCEPT_NAMES = ["aquamarine_left", "lava_1_above"]
 CONCEPT_PREFIXES = ["binary_concept_aquamarine_left", "binary_concept_lava_1_above"]
-VALIDATION_SET_TARGET_SIZES = [16_000, 170]
+VALIDATION_SET_TARGET_SIZES = [16_000, 24]
 CONCEPT_DATA_APPROACH_PATH_BASE = "rl_tcav_data/concept_examples/gem_collector/"
 VALIDATION_DATASET_PATHS = [
     "rl_tcav_data/cavs/baseline_concept_probes_experiment/gem_collector/validation_sets/aquamarine_left_probe_validation_dataset_method1.npy",
