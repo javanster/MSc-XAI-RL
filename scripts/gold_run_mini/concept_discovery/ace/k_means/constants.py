c864dfdf4c98e18a3aac1be84c15bb5b8618c33e
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-05 16:02:20 +0200

    Constants for k means concept discovery GC and GRM

---
+++ scripts/gold_run_mini/concept_discovery/ace/k_means/constants.py
@@ -0,0 +1,3 @@
+MAX_K = 50
+MODEL_OF_INTEREST_PATH = "models/GoldRunMini/sub-competent/firm-mountain-13/model_time_step_457199_episode_15800____0.4878avg____0.5000max____0.4532min.keras"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
