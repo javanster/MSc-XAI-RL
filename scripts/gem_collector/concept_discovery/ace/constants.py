570e8e89e1203e06579e1a4e9240455da4585317
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-05 22:19:25 +0200

    GC concept discovery constants

---
+++ scripts/gem_collector/concept_discovery/ace/constants.py
@@ -0,0 +1,3 @@
+MAX_CLUSTERS = 50
+MODEL_OF_INTEREST_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
