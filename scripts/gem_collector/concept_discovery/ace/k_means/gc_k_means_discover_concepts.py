667deeffc60c184ce1a200913fd5c995534d3380
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-05 22:19:44 +0200

    Make use of new constants

--- scripts/gem_collector/concept_discovery/ace/k_means/gc_k_means_discover_concepts.py
+++ scripts/gem_collector/concept_discovery/ace/k_means/gc_k_means_discover_concepts.py
@@ -4,7 +4,7 @@ from keras.api.saving import load_model
 from rl_ace import KMeansClusterer
 from utils import ModelActivationObtainer
 
-from .constants import MAX_K, MODEL_LAYERS_OF_INTEREST, MODEL_OF_INTEREST_PATH
+from ..constants import MAX_CLUSTERS, MODEL_LAYERS_OF_INTEREST, MODEL_OF_INTEREST_PATH
 
 
 def k_means_cluster(class_observations: np.ndarray, save_directory_path: str) -> None:
@@ -15,7 +15,7 @@ def k_means_cluster(class_observations: np.ndarray, save_directory_path: str) ->
 
     k_means_example_clusterer.cluster(
         environment_observations=class_observations,
-        max_k=MAX_K,
+        max_k=MAX_CLUSTERS,
         model_layer_indexes=MODEL_LAYERS_OF_INTEREST,
         save_directory_path=save_directory_path,
     )
