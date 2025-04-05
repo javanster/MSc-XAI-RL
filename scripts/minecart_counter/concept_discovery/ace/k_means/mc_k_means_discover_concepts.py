dacd273849d2407f06e60360c4e33ff408a8c6cc
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-05 16:49:55 +0200

    Constants for k means concept discovery MC

---
+++ scripts/minecart_counter/concept_discovery/ace/k_means/mc_k_means_discover_concepts.py
@@ -0,0 +1,54 @@
+import numpy as np
+from keras.api.saving import load_model
+
+from rl_ace import KMeansClusterer
+from utils import ModelActivationObtainer
+
+from .constants import MAX_K, MODEL_LAYERS_OF_INTEREST, MODEL_OF_INTEREST_PATH
+
+
+def k_means_cluster(class_observations: np.ndarray, save_directory_path: str) -> None:
+    moi: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    mao = ModelActivationObtainer(model=moi, input_normalization_type="image")
+
+    k_means_example_clusterer = KMeansClusterer(model_activation_obtainer=mao)
+
+    k_means_example_clusterer.cluster(
+        environment_observations=class_observations,
+        max_k=MAX_K,
+        model_layer_indexes=MODEL_LAYERS_OF_INTEREST,
+        save_directory_path=save_directory_path,
+    )
+
+
+if __name__ == "__main__":
+    target_classes = [
+        "balanced",
+        "up",
+        "upright",
+        "right",
+        "downright",
+        "down",
+        "downleft",
+        "left",
+        "upleft",
+    ]
+    class_obs_paths = [
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_balanced_30000_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_up_7070_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_upright_30000_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_right_22997_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_downright_11686_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_down_9680_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_downleft_7738_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_left_12383_shuffled_examples.npy",
+        "rl_concept_discovery_data/class_datasets_model_of_interest/minecart_counter/target_class_upleft_24696_shuffled_examples.npy",
+    ]
+
+    for target_class, class_obs_path in zip(target_classes, class_obs_paths):
+        class_observations = np.load(class_obs_path)
+
+        k_means_cluster(
+            class_observations=class_observations,
+            save_directory_path=f"rl_ace_data/concept_examples/k_means/minecart_counter/model_of_interest_target_class_{target_class}_observations/",
+        )
