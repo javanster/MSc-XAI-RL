9deef93019cd9f42431725445aa14a2cef63f64b
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-07 19:34:24 +0200

    Update path

--- scripts/gem_collector/tcav/concept_example_collection/iter_2/constants.py
+++ scripts/gem_collector/tcav/concept_example_collection/iter_2/constants.py
@@ -1,4 +1,6 @@
-EXAMPLE_DATA_DIRECTORY_PATH = "rl_tcav_data/concept_examples/by_concept_function_r2/gem_collector"
+EXAMPLE_DATA_DIRECTORY_PATH = (
+    "rl_tcav_data/concept_examples/gem_collector/by_concept_functions/iter_2"
+)
 ENV_NAME = "gem_collector"
 EXAMPLE_N = 5_000
 MAX_ITERATIONS = 100_000
