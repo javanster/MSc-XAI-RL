c84067bbb1d58f56a383896c2c4fd6a2abdd9518
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-07 20:25:30 +0200

    Update dir path

--- scripts/minecart_counter/tcav/concept_example_collection/iter_1/constants.py
+++ scripts/minecart_counter/tcav/concept_example_collection/iter_1/constants.py
@@ -1,3 +1,5 @@
-EXAMPLE_DATA_DIRECTORY_PATH = "rl_tcav_data/concept_examples/minecart_counter"
+EXAMPLE_DATA_DIRECTORY_PATH = (
+    "rl_tcav_data/concept_examples/minecart_counter/by_concept_functions/iter_1"
+)
 ENV_NAME = "minecart_counter"
 EXAMPLE_N = 100_000
