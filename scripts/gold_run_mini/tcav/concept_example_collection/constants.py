c87b9a974bb00693d0db78d4452262d153c55d9e
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-07 19:36:30 +0200

    GRM Concept example collection scripts iter 1

--- scripts/gold_run_mini/tcav/concept_example_collection/constants.py
+++
@@ -1,4 +0,0 @@
-EXAMPLE_DATA_DIRECTORY_PATH = "rl_tcav_data/concept_examples/gold_run_mini"
-ENV_NAME = "gold_run_mini"
-EXAMPLE_N = 200_000
-ENV_LAVA_SPOTS = 8
---
+++ scripts/gold_run_mini/tcav/concept_example_collection/iter_1/constants.py
@@ -0,0 +1,6 @@
+EXAMPLE_DATA_DIRECTORY_PATH = (
+    "rl_tcav_data/concept_examples/gold_run_mini/by_concept_functions/iter_1"
+)
+ENV_NAME = "gold_run_mini"
+EXAMPLE_N = 200_000
+ENV_LAVA_SPOTS = 8
