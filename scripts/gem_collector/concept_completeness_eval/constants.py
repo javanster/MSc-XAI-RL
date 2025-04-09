ed181658e7f318f0a509adc6aba60b7fef8f7fb6
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-09 16:30:07 +0200

    CCM dataset creation script GC

---
+++ scripts/gem_collector/concept_completeness_eval/constants.py
@@ -0,0 +1,4 @@
+MODEL_OF_INTEREST_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
+MODEL_OF_INTEREST_NAME = "denim-sweep-56"
+DATASETS_CCM_DIR_PATH = f"rl_ccm_data/gem_collector/{MODEL_OF_INTEREST_NAME}/"
