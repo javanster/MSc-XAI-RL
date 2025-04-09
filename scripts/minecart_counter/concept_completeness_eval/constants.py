c27cd7125e98e93fcc1565a3e92b9f9c4b66975e
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-09 16:29:39 +0200

    CCM dataset creation script MC

---
+++ scripts/minecart_counter/concept_completeness_eval/constants.py
@@ -0,0 +1,5 @@
+MODEL_OF_INTEREST_PATH = "models/MinecartCounter/scattered/kind-cosmos-35/1740021317_model____0.5122avg____0.9750max___-0.0900min.keras"
+MODEL_OF_INTEREST_NAME = "kind-cosmos-35"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
+DATASETS_CCM_DIR_PATH = f"rl_ccm_data/minecart_counter/{MODEL_OF_INTEREST_NAME}/"
+ENV_LAVA_SPOTS = 8
---
+++ scripts/minecart_counter/concept_completeness_eval/mc_save_ccm_datasets.py
@@ -0,0 +1,32 @@
+import gymnasium as gym
+import minecart_counter
+import numpy as np
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+
+from rl_ccm import get_base_and_eval_dataset
+from utils import ensure_directory_exists
+
+from .constants import DATASETS_CCM_DIR_PATH, MODEL_OF_INTEREST_PATH
+
+
+def mc_save_ccm_datasets():
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(
+        id="MinecartCounter-v2",
+        scatter_minecarts=True,
+    )
+
+    X_train, X_val, Y_train, Y_val = get_base_and_eval_dataset(
+        size=10_000, env=env, model=model, normalization_callback="image"
+    )
+
+    ensure_directory_exists(DATASETS_CCM_DIR_PATH)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_train_{len(X_train)}_examples.npy", X_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_val_{len(X_val)}_examples.npy", X_val)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_train_{len(Y_train)}_labels.npy", Y_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_val_{len(Y_val)}_labels.npy", Y_val)
+
+
+if __name__ == "__main__":
+    mc_save_ccm_datasets()
