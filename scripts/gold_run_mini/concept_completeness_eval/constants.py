825d37364d2c2bf1ee7079674657178468089908
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-09 16:30:32 +0200

    CCM dataset creation script GRM

---
+++ scripts/gold_run_mini/concept_completeness_eval/constants.py
@@ -0,0 +1,5 @@
+MODEL_OF_INTEREST_PATH = "models/GoldRunMini/sub-competent/firm-mountain-13/model_time_step_457199_episode_15800____0.4878avg____0.5000max____0.4532min.keras"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
+MODEL_OF_INTEREST_NAME = "firm-mountain-13"
+DATASETS_CCM_DIR_PATH = f"rl_ccm_data/gold_run_mini/{MODEL_OF_INTEREST_NAME}/"
+ENV_LAVA_SPOTS = 8
---
+++ scripts/gold_run_mini/concept_completeness_eval/grm_save_ccm_datasets.py
@@ -0,0 +1,32 @@
+import gold_run_mini
+import gymnasium as gym
+import numpy as np
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+
+from rl_ccm import get_base_and_eval_dataset
+from utils import ensure_directory_exists
+
+from .constants import DATASETS_CCM_DIR_PATH, ENV_LAVA_SPOTS, MODEL_OF_INTEREST_PATH
+
+
+def grm_save_ccm_datasets():
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(
+        id="GoldRunMini-v1",
+        lava_spots=ENV_LAVA_SPOTS,
+    )
+
+    X_train, X_val, Y_train, Y_val = get_base_and_eval_dataset(
+        size=10_000, env=env, model=model, normalization_callback="image"
+    )
+
+    ensure_directory_exists(DATASETS_CCM_DIR_PATH)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_train_{len(X_train)}_examples.npy", X_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_val_{len(X_val)}_examples.npy", X_val)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_train_{len(Y_train)}_labels.npy", Y_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_val_{len(Y_val)}_labels.npy", Y_val)
+
+
+if __name__ == "__main__":
+    grm_save_ccm_datasets()
