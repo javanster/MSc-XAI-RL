60c30d266e0346beac2011b1ac09f426f98f6635
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-10 10:20:12 +0200

    CCM eval GC

--- scripts/gem_collector/concept_completeness_eval/constants.py
+++ scripts/gem_collector/concept_completeness_eval/constants.py
@@ -1,4 +1,5 @@
 MODEL_OF_INTEREST_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
 MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
 MODEL_OF_INTEREST_NAME = "denim-sweep-56"
-DATASETS_CCM_DIR_PATH = f"rl_ccm_data/gem_collector/{MODEL_OF_INTEREST_NAME}/"
+DATASETS_CCM_DIR_PATH = f"rl_ccm_data/obs_action_set/gem_collector/{MODEL_OF_INTEREST_NAME}/"
+CCM_SCORES_DIR_PATH = f"rl_ccm_data/completeness_scores/gem_collector/{MODEL_OF_INTEREST_NAME}/"
---
+++ scripts/gem_collector/concept_completeness_eval/gc_ace_k_means_concept_ccm_eval.py
@@ -0,0 +1,69 @@
+import itertools
+
+import gem_collector
+import gymnasium as gym
+import numpy as np
+import pandas as pd
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+from tqdm import tqdm
+
+from rl_ccm import CCM
+from utils import ModelActivationObtainer, ensure_directory_exists
+
+from .constants import CCM_SCORES_DIR_PATH, MODEL_LAYERS_OF_INTEREST, MODEL_OF_INTEREST_PATH
+
+
+def eval_ace_k_means_concepts():
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(id="GemCollector-v3")
+    mao = ModelActivationObtainer(model=model, input_normalization_type="image")
+
+    X_train = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/X_train_8000_examples.npy"
+    )
+    X_val = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/X_val_2000_examples.npy"
+    )
+    Y_train = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/Y_train_8000_labels.npy"
+    )
+    Y_val = np.load("rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/Y_val_2000_labels.npy")
+
+    ccm = CCM(
+        model=model,
+        env=env,
+        model_activation_obtainer=mao,
+        X_train=X_train,
+        X_val=X_val,
+        Y_train=Y_train,
+        Y_val=Y_val,
+    )
+
+    k_values = [k for k in range(2, 51)]
+    results = []
+
+    with tqdm(unit="score", total=len(MODEL_LAYERS_OF_INTEREST) * len(k_values)) as pbar:
+        for layer_i, k in itertools.product(MODEL_LAYERS_OF_INTEREST, k_values):
+
+            cavs = np.load(
+                f"rl_ace_data/concept_examples/k_means/gem_collector/model_of_interest_target_class_balanced_observations/layer_{layer_i}/k_{k}_cluster_centroids.npy"
+            )
+
+            completeness_score = ccm.train_and_eval_ccm(
+                cavs=cavs,
+                conv_handling="dim_reduction",
+                layer_i=layer_i,
+                max_ccm_depth=5,
+            )
+
+            results.append({"layer": layer_i, "k": k, "completeness_score": completeness_score})
+            pbar.update(1)
+
+    df = pd.DataFrame(results)
+    ensure_directory_exists(directory_path=CCM_SCORES_DIR_PATH)
+    df.to_csv(f"{CCM_SCORES_DIR_PATH}ace_k_means_completeness_scores.csv", index=False)
+
+
+if __name__ == "__main__":
+    eval_ace_k_means_concepts()
