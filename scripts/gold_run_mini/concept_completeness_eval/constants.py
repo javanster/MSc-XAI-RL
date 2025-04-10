6fad5a6f29f1f6f2821c0d94cbed8feb31136a59
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-10 10:18:15 +0200

    CCM eval GRM

--- scripts/gold_run_mini/concept_completeness_eval/constants.py
+++ scripts/gold_run_mini/concept_completeness_eval/constants.py
@@ -1,5 +1,6 @@
 MODEL_OF_INTEREST_PATH = "models/GoldRunMini/sub-competent/firm-mountain-13/model_time_step_457199_episode_15800____0.4878avg____0.5000max____0.4532min.keras"
 MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
 MODEL_OF_INTEREST_NAME = "firm-mountain-13"
-DATASETS_CCM_DIR_PATH = f"rl_ccm_data/gold_run_mini/{MODEL_OF_INTEREST_NAME}/"
+DATASETS_CCM_DIR_PATH = f"rl_ccm_data/obs_action_set/gold_run_mini/{MODEL_OF_INTEREST_NAME}/"
 ENV_LAVA_SPOTS = 8
+CCM_SCORES_DIR_PATH = f"rl_ccm_data/completeness_scores/gold_run_mini/{MODEL_OF_INTEREST_NAME}/"
---
+++ scripts/gold_run_mini/concept_completeness_eval/grm_manual_concept_ccm_eval.py
@@ -0,0 +1,88 @@
+import os
+
+import gold_run_mini
+import gymnasium as gym
+import numpy as np
+import pandas as pd
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+from tqdm import tqdm
+
+from rl_ccm import CCM
+from utils import ModelActivationObtainer, ensure_directory_exists
+
+from .constants import (
+    CCM_SCORES_DIR_PATH,
+    ENV_LAVA_SPOTS,
+    MODEL_LAYERS_OF_INTEREST,
+    MODEL_OF_INTEREST_PATH,
+)
+
+
+def eval_manual_concepts():
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(
+        id="GoldRunMini-v1",
+        lava_spots=ENV_LAVA_SPOTS,
+    )
+    mao = ModelActivationObtainer(model=model, input_normalization_type="image")
+
+    X_train = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/X_train_8000_examples.npy"
+    )
+    X_val = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/X_val_2000_examples.npy"
+    )
+    Y_train = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/Y_train_8000_labels.npy"
+    )
+    Y_val = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/Y_val_2000_labels.npy"
+    )
+
+    ccm = CCM(
+        model=model,
+        env=env,
+        model_activation_obtainer=mao,
+        X_train=X_train,
+        X_val=X_val,
+        Y_train=Y_train,
+        Y_val=Y_val,
+    )
+
+    results = []
+
+    with tqdm(unit="score", total=len(MODEL_LAYERS_OF_INTEREST)) as pbar:
+        for layer_i in MODEL_LAYERS_OF_INTEREST:
+
+            cavs = []
+
+            concept_probe_path = (
+                "rl_tcav_data/concept_probes/gold_run_mini/completeness_testing/firm-mountain-13/"
+            )
+            for filename in sorted(os.listdir(concept_probe_path)):
+                if filename.endswith(f"_layer_{layer_i}_concept_probe.keras"):
+                    probe: Sequential = load_model(f"{concept_probe_path}{filename}")  # type: ignore
+                    weights, _ = probe.layers[0].get_weights()
+                    cav = weights.flatten()
+                    cavs.append(cav)
+
+            cavs = np.array(cavs)
+
+            completeness_score = ccm.train_and_eval_ccm(
+                cavs=cavs,
+                conv_handling="flatten",
+                layer_i=layer_i,
+                max_ccm_depth=5,
+            )
+
+            results.append({"layer": layer_i, "completeness_score": completeness_score})
+            pbar.update(1)
+
+    df = pd.DataFrame(results)
+    ensure_directory_exists(directory_path=CCM_SCORES_DIR_PATH)
+    df.to_csv(f"{CCM_SCORES_DIR_PATH}manual_concepts_completeness_scores.csv", index=False)
+
+
+if __name__ == "__main__":
+    eval_manual_concepts()
