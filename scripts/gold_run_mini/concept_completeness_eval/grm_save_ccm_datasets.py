894e4196c289b2018d715dc4ec75b80cad87807f
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-11 14:15:22 +0200

    Update CCM datasets scripts

--- scripts/gold_run_mini/concept_completeness_eval/grm_save_ccm_datasets.py
+++ scripts/gold_run_mini/concept_completeness_eval/grm_save_ccm_datasets.py
@@ -27,6 +27,16 @@ def grm_save_ccm_datasets():
     np.save(f"{DATASETS_CCM_DIR_PATH}Y_train_{len(Y_train)}_labels.npy", Y_train)
     np.save(f"{DATASETS_CCM_DIR_PATH}Y_val_{len(Y_val)}_labels.npy", Y_val)
 
+    X_train, X_val, Y_train, Y_val = get_base_and_eval_dataset(
+        size=10_000, env=env, model=model, normalization_callback="image", all_q=True
+    )
+
+    ensure_directory_exists(DATASETS_CCM_DIR_PATH)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_train_{len(X_train)}_all_q_examples.npy", X_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}X_val_{len(X_val)}_all_q_examples.npy", X_val)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_train_{len(Y_train)}_all_q_labels.npy", Y_train)
+    np.save(f"{DATASETS_CCM_DIR_PATH}Y_val_{len(Y_val)}_all_q_labels.npy", Y_val)
+
 
 if __name__ == "__main__":
     grm_save_ccm_datasets()
