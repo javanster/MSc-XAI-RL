877caa7a3dabf223293150529bdd0b75b662ef66
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-11 10:56:10 +0200

    k means constants MC

---
+++ scripts/minecart_counter/concept_discovery/ace/k_means/constants.py
@@ -0,0 +1,3 @@
+MAX_K = 50
+MODEL_OF_INTEREST_PATH = "models/MinecartCounter/scattered/kind-cosmos-35/1740021317_model____0.5122avg____0.9750max___-0.0900min.keras"
+MODEL_LAYERS_OF_INTEREST = [0, 1, 3, 4, 5]  # Not including flatten layer and output layer
