639b4306edfa0e427dafa63c299fa8987dd5cb6e
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-04-15 19:44:49 +0200

    ICE CCM concept eval scripts

---
+++ scripts/gem_collector/concept_completeness_eval/gc_ice_concept_ccm_nn_eval.py
@@ -0,0 +1,90 @@
+from typing import cast
+
+import gem_collector
+import gymnasium as gym
+import numpy as np
+import pandas as pd
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+from tqdm import tqdm
+
+from rl_ccm import CCM_NN
+from utils import ModelActivationObtainer, ensure_directory_exists
+
+from .constants import CCM_SCORES_DIR_PATH, MODEL_OF_INTEREST_PATH
+
+
+def eval_ice_concepts_nn():
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(id="GemCollector-v3")
+    mao = ModelActivationObtainer(model=model, input_normalization_type="image")
+
+    X_train = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/X_train_8000_examples.npy"
+    )
+    X_val = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/X_val_2000_examples.npy"
+    )
+    Y_train = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/Y_all_q_train_8000_examples.npy"
+    )
+    Y_val = np.load(
+        "rl_ccm_data/obs_action_set/gem_collector/denim-sweep-56/Y_all_q_val_2000_examples.npy"
+    )
+
+    ccm = CCM_NN(
+        num_classes=env.action_space.n,
+        model_activation_obtainer=mao,
+        X_train=X_train,
+        X_val=X_val,
+        Y_train=Y_train,
+        Y_val=Y_val,
+        all_q=True,
+    )
+
+    c_values = [c for c in range(2, 51)]
+    results = []
+    completeness_scores = []
+    nns = []
+
+    with tqdm(unit="score", total=len(c_values)) as pbar:
+        for c in c_values:
+
+            cavs = np.load(
+                f"rl_ice_data/ncavs/gem_collector/model_of_interest_target_class_balanced_observations/layer_1/c_{c}_ncavs.npy"
+            )
+
+            completeness_score, nn = ccm.train_and_eval_ccm(
+                cavs=cavs,
+                conv_handling="dim_reduction",
+                layer_i=1,
+            )
+
+            completeness_scores.append(completeness_score)
+
+            nn = cast(Sequential, nn)
+            nns.append(nn)
+
+            results.append({"layer": 1, "c": c, "completeness_score": completeness_score})
+            pbar.update(1)
+
+    bcsi = np.argmax(completeness_scores)
+    best_nn = nns[bcsi]
+    best_result = results[bcsi]
+    best_c = best_result["c"]
+    best_layer = best_result["layer"]
+
+    save_path = f"{CCM_SCORES_DIR_PATH}/ccm_nn/"
+
+    ensure_directory_exists(directory_path=save_path)
+
+    best_nn.save(
+        f"{save_path}best_ccm_ice_layer_{best_layer}_c_{best_c}.keras",
+    )
+
+    df = pd.DataFrame(results)
+    df.to_csv(f"{save_path}ice_completeness_scores_all_q.csv", index=False)
+
+
+if __name__ == "__main__":
+    eval_ice_concepts_nn()
---
+++ scripts/gold_run_mini/concept_completeness_eval/grm_ice_concept_ccm_dt_eval.py
@@ -0,0 +1,97 @@
+import itertools
+
+import gold_run_mini
+import gymnasium as gym
+import joblib
+import numpy as np
+import pandas as pd
+from keras.api.models import Sequential
+from keras.api.saving import load_model
+from tqdm import tqdm
+
+from rl_ccm import CCM_DT
+from utils import ModelActivationObtainer, ensure_directory_exists
+
+from .constants import CCM_SCORES_DIR_PATH, ENV_LAVA_SPOTS, MODEL_OF_INTEREST_PATH
+
+
+def eval_ice_concepts_ccm_dt(layer_i: int, c: int):
+    model: Sequential = load_model(MODEL_OF_INTEREST_PATH)  # type: ignore
+    env = gym.make(
+        id="GoldRunMini-v1",
+        lava_spots=ENV_LAVA_SPOTS,
+    )
+    mao = ModelActivationObtainer(model=model, input_normalization_type="image")
+
+    X_train = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/X_train_8000_examples.npy"
+    )
+    X_val = np.load(
+        "rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/X_val_2000_examples.npy"
+    )
+
+    results = []
+
+    save_path = f"{CCM_SCORES_DIR_PATH}/ccm_dt/"
+
+    ensure_directory_exists(directory_path=save_path)
+
+    target_types = ["max_q", "all_q"]
+    max_depths = [3, 4, 5]
+
+    with tqdm(total=len(target_types) * len(max_depths), unit="score") as pbar:
+        for target_type, max_depth in itertools.product(target_types, max_depths):
+
+            Y_train = np.load(
+                f"rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/Y_{target_type}_train_8000_examples.npy"
+            )
+            Y_val = np.load(
+                f"rl_ccm_data/obs_action_set/gold_run_mini/firm-mountain-13/Y_{target_type}_val_2000_examples.npy"
+            )
+
+            ccm = CCM_DT(
+                num_classes=env.action_space.n,
+                model_activation_obtainer=mao,
+                X_train=X_train,
+                X_val=X_val,
+                Y_train=Y_train,
+                Y_val=Y_val,
+                all_q=True if target_type == "all_q" else False,
+                max_depth=max_depth,
+            )
+
+            cavs = np.load(
+                f"rl_ice_data/ncavs/gold_run_mini/model_of_interest_target_class_balanced_observations/layer_{layer_i}/c_{c}_ncavs.npy"
+            )
+
+            completeness_score, model = ccm.train_and_eval_ccm(
+                cavs=cavs,
+                conv_handling="dim_reduction",
+                layer_i=layer_i,
+                use_sigmoid=[False for _ in range(len(cavs))],  # All concepts are continuous
+                biases=None,
+            )
+
+            results.append(
+                {
+                    "layer": layer_i,
+                    "c": c,
+                    "target_type": target_type,
+                    "max_depth": max_depth,
+                    "completeness_score": completeness_score,
+                }
+            )
+
+            joblib.dump(
+                model,
+                f"{save_path}best_ccm_ice_layer_{layer_i}_c_{c}_{target_type}_max_depth_{max_depth}.joblib",
+            )
+
+            pbar.update(1)
+
+    df = pd.DataFrame(results)
+    df.to_csv(f"{save_path}ice_concepts_completeness_scores.csv", index=False)
+
+
+if __name__ == "__main__":
+    eval_ice_concepts_ccm_dt(layer_i=1, c=47)  # Based on best layer of nn ccm
