ad7bd1033f5b00bbfd1e16c5cd88feaa5ae3ed4c
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2025-05-08 10:33:17 +0200

    Script updates

--- scripts/gem_collector/gc_ddqn_test.py
+++ scripts/gem_collector/gc_ddqn_test.py
@@ -1,12 +1,13 @@
 import argparse
-from typing import cast
+from typing import Any, Dict, cast
 
 import gem_collector
 import gymnasium as gym
+import numpy as np
 from keras.api.models import Sequential
 from keras.api.saving import load_model
 
-from agents import DDQNAgent
+from agents import DDQNAgent, TestLogger
 
 OPTIMAL_POLICY_MODEL_FILE_PATH = "models/GemCollector/iconic-sweep-77/1738860819_model____0.6935avg____0.7910max____0.4690min.keras"
 SUB_OPTIMAL_POLICY_MODEL_FILE_PATH = "models/GemCollector/denim-sweep-56/1738828803_model____0.3766avg____0.6576max____0.1570min.keras"
@@ -26,18 +27,46 @@ def parse_arguments():
 if __name__ == "__main__":
     args = parse_arguments()
 
-    model_path = (
-        OPTIMAL_POLICY_MODEL_FILE_PATH
-        if args.model == "optimal"
-        else SUB_OPTIMAL_POLICY_MODEL_FILE_PATH
-    )
+    log_vars = {
+        "aquamarines_collected_count": 0,
+        "amethysts_collected_count": 0,
+        "emeralds_collected_count": 0,
+        "rocks_collected_count": 0,
+        "lava_termination_count": 0,
+        "truncated_count": 0,
+        "total_reward": 0,
+    }
+
+    def step_callback(
+        observation: np.ndarray,
+        reward: float,
+        terminated: bool,
+        truncated: bool,
+        info: Dict[Any, Any],
+        log_vars: Dict[str, Any],
+    ):
+        log_vars["aquamarines_collected_count"] += info["aquamarine_collected"]
+        log_vars["amethysts_collected_count"] += info["amethyst_collected"]
+        log_vars["emeralds_collected_count"] += info["emerald_collected"]
+        log_vars["rocks_collected_count"] += info["rocks_collected"]
+        log_vars["lava_termination_count"] += 1 if info["lava_collision"] else 0
+        log_vars["truncated_count"] += 1 if truncated else 0
+        log_vars["total_reward"] += info["reward"]
 
     print(f"Testing with {args.model} model")
 
     env = gym.make(id="GemCollector-v3", render_mode="human", show_raw_pixels=False)
     agent = DDQNAgent(env=env, obervation_normalization_type="image")
 
-    model = load_model(filepath=model_path)
+    model = load_model(filepath=OPTIMAL_POLICY_MODEL_FILE_PATH)
     model = cast(Sequential, model)
 
-    agent.test(model=model, episodes=1)
+    test_logger = TestLogger(log_vars=log_vars, step_callback=step_callback)
+
+    EPISODES = 1
+
+    agent.test(model=model, episodes=EPISODES, test_logger=test_logger, epsilon=-1)
+
+    print(log_vars)
+
+    print(f"Avg reward per episode: {log_vars['total_reward'] / EPISODES}")
--- scripts/gem_collector/gc_play_op.py
+++ scripts/gem_collector/gc_play_op.py
@@ -1,8 +1,9 @@
 import gem_collector
 import gymnasium as gym
+from tqdm import tqdm
 
 if __name__ == "__main__":
-    env = gym.make(id="GemCollector-v3", render_mode="human", show_raw_pixels=False)
+    env = gym.make(id="GemCollector-v3", render_mode=None, show_raw_pixels=False)
 
     def get_optimal_action(step: int) -> int:
         """
@@ -10,14 +11,39 @@ if __name__ == "__main__":
         """
         return 1 - (step // 19) % 2
 
-    for _ in range(1):
-        step_n = 0
-        terminated = False
-        observation, _ = env.reset()
-        while not terminated:
-            env.render()
-            action = get_optimal_action(step=step_n)
+    log_vars = {
+        "aquamarines_collected_count": 0,
+        "amethysts_collected_count": 0,
+        "emeralds_collected_count": 0,
+        "rocks_collected_count": 0,
+        "lava_termination_count": 0,
+        "truncated_count": 0,
+        "total_reward": 0,
+    }
 
-            observation, reward, terminated, truncated, _ = env.step(action)
+    EPISODES = 5000
 
-            step_n += 1
+    with tqdm(total=EPISODES, unit="episode") as pbar:
+        for _ in range(EPISODES):
+            step_n = 0
+            terminated = False
+            observation, _ = env.reset()
+            while not terminated:
+                env.render()
+                action = get_optimal_action(step=step_n)
+
+                observation, reward, terminated, truncated, info = env.step(action)
+
+                step_n += 1
+
+                log_vars["aquamarines_collected_count"] += info["aquamarine_collected"]
+                log_vars["amethysts_collected_count"] += info["amethyst_collected"]
+                log_vars["emeralds_collected_count"] += info["emerald_collected"]
+                log_vars["rocks_collected_count"] += info["rocks_collected"]
+                log_vars["lava_termination_count"] += 1 if info["lava_collision"] else 0
+                log_vars["truncated_count"] += 1 if truncated else 0
+                log_vars["total_reward"] += info["reward"]
+
+            pbar.update(1)
+
+    print(log_vars)
